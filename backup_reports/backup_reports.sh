21f99e8ee8fe43af66da3ee7a890d9fbed3c3c41
BhavanaBollampally <bhavanabollampally3@gmail.com>
2025-10-06 20:56:54 +0530

    backup logs on

--- backup_reports/backup_reports.sh
+++ backup_reports/backup_reports.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-cp ~/devops_practice/reports/log1.txt ~/devops_practice/backup_reports/$(date + %Y%m%d)_log1.txt
-cp ~/devops_practice/reports/log2.txt ~/devops_practice/backup_reports/$(date + %Y%m%d)_log2.txt
+cp ~/devops_practice/reports/log1.txt ~/devops_practice/backup_reports/$(date + "%Y%m%d")_log1.txt
+cp ~/devops_practice/reports/log2.txt ~/devops_practice/backup_reports/$(date + "%Y%m%d")_log2.txt
 cd ~/devops_practice
 git init
 git add .
